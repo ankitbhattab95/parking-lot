fa36e98ddea6c365854d5d97891ea6b943883f14
Your Name <ankit.bhatt@playerzpot.com>
2021-06-06 17:10:02 +0530

    Comment out unit test example

--- app/src/test/person.test.js
+++ app/src/test/person.test.js
@@ -1,28 +1,34 @@
-const { connectMongoDb, closeMongoDB, clearMongoDB } = require("../loaders/database-loader")
-const PersonService = require("../services/person-service")
-const personService = new PersonService()
+// ----------------------------------------------
+// ----------------------------------------------
+// This is an example unit test
+// ----------------------------------------------
+// ----------------------------------------------
 
-beforeAll(async () => await connectMongoDb())
-afterEach(async () => await clearMongoDB())
-afterAll(async () => await closeMongoDB())
+// const { connectMongoDb, closeMongoDB, clearMongoDB } = require("../loaders/database-loader")
+// const PersonService = require("../services/person-service")
+// const personService = new PersonService()
 
-test("insert a person", async (done) => {
-  const body = { email: "samsam" }
-  try {
-    const personDetails = await personService.create(body)
-    expect(personDetails).toHaveProperty("id")
-    done()
-  } catch (error) {
-    done()
-  }
-})
+// beforeAll(async () => await connectMongoDb())
+// afterEach(async () => await clearMongoDB())
+// afterAll(async () => await closeMongoDB())
 
-test("insert a person - error case", async (done) => {
-  const body = { email: "damdda" }
-  try {
-    const personDetails = await personService.create(body)
-    expect(personDetails).toHaveProperty("__vv")
-  } catch (error) {
-    done()
-  }
-})
+// test("insert a person", async (done) => {
+//   const body = { email: "samsam" }
+//   try {
+//     const personDetails = await personService.create(body)
+//     expect(personDetails).toHaveProperty("id")
+//     done()
+//   } catch (error) {
+//     done()
+//   }
+// })
+
+// test("insert a person - error case", async (done) => {
+//   const body = { email: "damdda" }
+//   try {
+//     const personDetails = await personService.create(body)
+//     expect(personDetails).toHaveProperty("__vv")
+//   } catch (error) {
+//     done()
+//   }
+// })
